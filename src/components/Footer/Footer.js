fb2a1d6e0a984fba3f1f92979bcf62817eeada7a
Amory <garbage90@yandex.ru>
2021-02-17 22:44:41 +0300

    feat: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Footer.css';
 
 function Footer() {
   return(
     <footer className="footer">
-      
+      <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className="footer__info">
+        <p className="footer__copyright">&copy; {new Date().getFullYear()}</p>
+        <ul className="footer__links">
+          <li><Link to="/" className="footer__link">Яндекс.Практикум</Link></li>
+          <li><Link to="/" className="footer__link">Github</Link></li>
+          <li><Link to="/" className="footer__link">Facebook</Link></li>
+        </ul>
+      </div>
     </footer>
   )
 }
