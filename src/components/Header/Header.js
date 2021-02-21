077ceb934477f7f219dc0af9d862acdf11539ecd
Amory <garbage90@yandex.ru>
2021-02-21 11:39:46 +0300

    feat: movies, profile _ links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import './Header.css';
 import logo from '../../images/logo.svg';
+import profile from '../../images/profile.svg'
 import { NavLink } from 'react-router-dom';
 
 function Header() {
@@ -8,7 +9,13 @@ function Header() {
       <NavLink to="/" className="logo"><img src={logo} alt="Логотип"/></NavLink>
       <nav className="header__menu">
         <NavLink to="/" className="header__menu-item">Регистрация</NavLink>
+        <NavLink to="/movies" className="header__menu-item">Фильмы</NavLink>
+        <NavLink to="/movies" className="header__menu-item">Сохранённые фильмы</NavLink>
         <button className="header__signin">Войти</button>
+        <button className="header__profile">
+          <p className="header__profile-text">Аккаунт</p>
+          <img src={profile} alt="Аккаунт" className="header__profile-img"/>
+        </button>
       </nav>
     </header>
   )
