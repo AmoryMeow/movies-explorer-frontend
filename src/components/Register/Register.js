c2fa0d378caa1ac9d7c81fd802a9217bebafcc71
Amory <garbage90@yandex.ru>
2021-02-21 16:54:58 +0300

    refactor: Register with AuthForm

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,13 +2,21 @@ import './Register.css';
 import Input from '../Input/Input';
 import logo from '../../images/logo.svg';
 import { NavLink } from 'react-router-dom';
+import AuthForm from '../AuthForm/AuthForm';
 
 function Register() {
   return (
     <section className="register">
       <NavLink to="/" className="logo"><img src={logo} alt="Логотип"/></NavLink>
       <h2 className="register__title">Добро пожаловать!</h2>
-      <form className="register__form" action="post" name="register" noValidate>
+
+      <AuthForm
+        name="register"
+        submitText="Зарегистрироваться"
+        linkText="Войти"
+        linkSubText="Уже зарегистрированы?"
+        link="/signin"
+      >
         <Input
           id="name"
           name="name"
@@ -27,10 +35,9 @@ function Register() {
           type="password"
           placeholder="Пароль"
           errorText="Что-то пошло не так..."
-        />  
-        <button className="register__submit">Зарегистрироваться</button>
-        <p className="register__text">Уже зарегистрированы? <NavLink className="register__link" to="/signin">Войти</NavLink></p>
-      </form>
+        /> 
+      </AuthForm>
+
     </section>
   );
 }
