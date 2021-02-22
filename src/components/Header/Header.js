0f62919ac1ca1ead71064a964e7b1e4856f625a7
Amory <garbage90@yandex.ru>
2021-02-22 18:27:59 +0300

    refactor: Header - adaptive layout, hidden menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,50 @@
+import { NavLink } from 'react-router-dom';
+import React from 'react';
 import './Header.css';
 import logo from '../../images/logo.svg';
 import profile from '../../images/profile.svg'
-import { NavLink } from 'react-router-dom';
+
 
 function Header() {
+  
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
+  
+  function handleOpenMenu() {
+    setMenuIsOpen(true);
+  }
+
+  function handleCloseMenu() {
+    setMenuIsOpen(false);
+  }
   return(
     <header className="header">
       <NavLink to="/" className="logo"><img src={logo} alt="Логотип"/></NavLink>
-      <nav className="header__menu">
-        <NavLink to="/signup" className="header__menu-item">Регистрация</NavLink>
-        <NavLink to="/movies" className="header__menu-item">Фильмы</NavLink>
-        <NavLink to="/saved-movies" className="header__menu-item">Сохранённые фильмы</NavLink>
-        <NavLink to="/signin" className="header__signin">Войти</NavLink>
-        <NavLink to="/profile" className="header__profile">
-          <p className="header__profile-text">Аккаунт</p>
-          <img src={profile} alt="Аккаунт" className="header__profile-img"/>
-        </NavLink>
-      </nav>
+
+      <div className="header__container"> 
+        <nav className="header__auth ">
+          <NavLink to="/signup" className="header__signup">Регистрация</NavLink>
+          <NavLink to="/signin" className="header__signin">Войти</NavLink>
+        </nav>
+        <div className={`header__cover ${!menuIsOpen && 'header__cover_hidden'}`}>
+          <nav className="header__menu">
+            <NavLink to="/" className="header__menu-item">Главная</NavLink>
+            <NavLink to="/movies" className="header__menu-item" activeClassName="header__menu-item_active">Фильмы</NavLink>
+            <NavLink to="/saved-movies" className="header__menu-item" activeClassName="header__menu-item_active">Сохранённые фильмы</NavLink>
+            <NavLink to="/profile" className="header__profile">
+              <p className="header__profile-text">Аккаунт</p>
+              <img src={profile} alt="Аккаунт" className="header__profile-img"/>
+            </NavLink>
+            <div className="header__close-menu" onClick={handleCloseMenu}>
+              <div className="header__cross"></div>
+            </div>
+          </nav>
+        </div>
+        <div className="header__open-menu" onClick={handleOpenMenu}>
+          <div className="header__line"></div>
+        </div>
+        
+      </div>
+      
     </header>
   )
 }
