bff6a411f69f3b5f69cf2ad9d35c73d598d6432b
Amory <garbage90@yandex.ru>
2021-02-26 18:17:06 +0300

    refactor: Profile - adaptive layout

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -26,18 +26,19 @@ function Profile() {
               placeholder="Почта" required minlength="2" maxlength="30" disabled={viewMode}/>
           </label>
         </article>
-        {viewMode ? (
-          <>
-            <button className="profile__button profile__button_type_edit" onClick={handleEditClick}>Редактировать</button>
-            <button className="profile__button profile__button_type_exit">Выйти из аккаунта</button>
-          </>
-        ) :(
-          <>
-            <span class="profile__error" id="name-input-error">ошибка</span>
-            <button className="profile__button profile__button_type_save">Сохранить</button>
-          </>
-        )}
-               
+        <div className="profile__buttons">
+          {viewMode ? (
+            <>
+              <button className="profile__button profile__button_type_edit" onClick={handleEditClick}>Редактировать</button>
+              <button className="profile__button profile__button_type_exit">Выйти из аккаунта</button>
+            </>
+          ) :(
+            <>
+              <span class="profile__error" id="name-input-error">ошибка</span>
+              <button className="profile__button profile__button_type_save">Сохранить</button>
+            </>
+          )}
+        </div>     
       </form>
     </section>
   );
