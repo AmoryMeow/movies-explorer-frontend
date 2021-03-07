c4695b1b94058b4621939324048b7274106934a8
Amory <garbage90@yandex.ru>
2021-03-07 09:34:24 +0300

    fix: ProtectedRoute check loggedIn

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -4,7 +4,7 @@ function ProtectedRoute({component: Component, ...props}) {
   return (
     <Route>
       {
-        () => props.loggedIn ? <Component {...props} /> : <Redirect to="./signin" />        
+        () => localStorage.getItem('token') || props.loggedIn ? <Component {...props} /> : <Redirect to="./signin" />        
       }
     </Route>
   )
